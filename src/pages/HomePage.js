c9778cf06eea9d3929089b9893b0197a19bf051f
PenHsuanWang <willy118116@gmail.com>
2023-10-07 12:52:08 +0800

    update home page layout

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,38 @@
 import React from 'react';
-import HomePageLayout from '../layouts/HomePageLayout'; // Import the HomePageLayout component
+import { Link } from 'react-router-dom';
+import '../styles/HomePage.css'; // Import the CSS file
+import logo from '../logo.svg'; // Import your images
 
 function HomePage() {
+  const images = [
+    { id: 1, src: logo, alt: 'Logo' },
+    { id: 2, src: logo, alt: 'Logo' },
+    { id: 3, src: logo, alt: 'Logo' },
+    { id: 4, src: logo, alt: 'Logo' },
+  ];
+
   return (
-    <HomePageLayout>
-      <div>
-        <h1>Welcome to the Home Page</h1>
-        <p>This is the home page content. You can customize it as needed.</p>
+    <div className="App">
+      <header className="App-header">Website Logo & Navigation</header>
+      <nav>Navigation Bar / Menu</nav>
+
+      <div className="App-content">
+        <main className="Main-content home-page">
+          <h1>Welcome to the Home Page</h1>
+          <p>This is the home page content. You can customize it as needed.</p>
+
+          <div className="image-grid">
+            {images.map((image) => (
+              <Link to="/stock-ma-plot" key={image.id} className="grid-item">
+                <img src={image.src} alt={image.alt} />
+              </Link>
+            ))}
+          </div>
+        </main>
       </div>
-    </HomePageLayout>
+
+      <footer>Footer Content</footer>
+    </div>
   );
 }
 
