f142fce743a274027f62104b7dfc3fa3af52e5e0
PenHsuanWang <willy118116@gmail.com>
2023-10-22 14:52:06 +0800

    refactor page with props passing components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import the necessary components
-import StockDiagramPage from './pages/StockDiagramPage.js';
-import HomePage from './pages/HomePage.js'; // Import the HomePage component
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage.js';
 import BasePage from './pages/BasePage.js';
 import DataVisualizationPage from './pages/DataVisualizationPage';
+
+import StockDiagram from './components/charts/StockDiagram';
+import TwoDHeatmapDiagram from './components/charts/TwoDHeatmapDiagram';
+
+import DatasetFromListControls from './components/containers/ListDatasetFromDBControls';
+import StockSearchControls from './components/containers/StockSearchControls';
+
 import './styles/BasePage.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Define your routes */}
         <Route path="/" element={<HomePage />} />
-        <Route path="/stock-ma-plot" element={<DataVisualizationPage />} />
+        
+        <Route 
+          path="/stock-ma-plot" 
+          element={
+            <DataVisualizationPage 
+              ChartComponent={StockDiagram} 
+              ControlComponent={StockSearchControls}
+            />
+          } 
+        />
+        
+        <Route 
+          path="/2d-heatmap-dataset" 
+          element={
+            <DataVisualizationPage 
+              ChartComponent={TwoDHeatmapDiagram} 
+              ControlComponent={DatasetFromListControls}
+            />
+          } 
+        />
+
         <Route path="/base" element={<BasePage />} />
-        {/* Add other routes here */}
       </Routes>
     </Router>
   );
